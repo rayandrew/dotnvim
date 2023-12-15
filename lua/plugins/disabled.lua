36128cdb5ca872f679316a7730ece0d830ef1dba
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-12-14 18:03:42 -0600

    update

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,13 +2,5 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "stevearc/dressing.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
-  {
-    "folke/noice.nvim",
-    opts = {
-      notify = {
-        enabled = false,
-      },
-    },
-  },
   { "rcarriga/nvim-notify", enabled = false },
 }
--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -25,6 +25,19 @@ return {
     },
   },
 
+  -- {
+  --   "folke/noice.nvim",
+  --   opts = {
+  --     presets = {
+  --       bottom_search = false, -- use a classic bottom cmdline for search
+  --       command_palette = false, -- position the cmdline and popupmenu together
+  --       long_message_to_split = true, -- long messages will be sent to a split
+  --       inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --       lsp_doc_border = false, -- add a border to hover docs and signature help
+  --     },
+  --   },
+  -- },
+
   {
     "3rd/image.nvim",
     opts = {
